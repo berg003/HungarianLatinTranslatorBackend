8ad498395c1a34613f12d7f2bd9a4f8d52aaff21
Alexander Berg <alexander.berg.p92@sonymusic.com>
2019-09-27 02:29:20 +0200

    minor fix

--- hu_la.php
+++ hu_la.php
@@ -2,7 +2,7 @@
 	// Include config.php
 	include_once('config.php');
 
-	$hu = isset($_GET['hu']) ? $mysqli->real_escape_string(utf8_decode(urldecode($_GET['hu']))) :  "";	
+	$hu = isset($_GET['hu']) ? $mysqli->real_escape_string($_GET['hu']) :  "";	
 	if(!empty($hu)){
 		if ($result = $mysqli->query("SELECT latin_expression FROM hungarian_latin WHERE hungarian_expression='$hu'")) {
 			while($rows = $result->fetch_array(MYSQLI_ASSOC)) {
